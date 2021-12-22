c75729e6a9292a028af9ad31fbc2b4f2034ed7eb
Sebastiaan Mannem <sebas@mannem.nl>
2021-12-22 23:17:22 +0100

    detecting version and also scanning war files

--- go.mod
+++ go.mod
@@ -2,13 +2,4 @@ module github.com/1lann/log4shelldetect
 
 go 1.17
 
-require (
-	github.com/fatih/color v1.13.0
-	github.com/karrick/godirwalk v1.16.1
-)
-
-require (
-	github.com/mattn/go-colorable v0.1.9 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
-)
+require github.com/hashicorp/go-version v1.3.0
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"archive/zip"
 	"bytes"
+	"crypto/sha256"
 	"errors"
 	"flag"
 	"fmt"
+    "github.com/hashicorp/go-version"
 	"io"
 	"log"
 	"math"
@@ -16,76 +18,87 @@ import (
 	"sync"
 )
 
-var printMutex = new(sync.Mutex)
-
-type status int
-
-func (s status) String() (ret string) {
-
-	switch s {
-	case StatusOK:
-		return "OK"
-	case StatusPatched:
-		return "PATCHED"
-	case StatusVulnerable:
-		return "VULNRBL"
-	case StatusMaybe:
-		return "MAYBE"
-	case StatusUnknown:
-		return "UNKNOWN"
+var (
+	jndiLookupClassVersionHash = map[string]string{
+		"39a495034d37c7934b64a9aa686ea06b61df21aa222044cc50a47d6903ba1ca8": "2.0-beta9",
+		"a03e538ed25eff6c4fe48aabc5514e5ee687542f29f2206256840e74ed59bcd2": "2.0-rc2",
+		"fd6c63c11f7a6b52eff04be1de3477c9ddbbc925022f7216320e6db93f1b7d29": "2.0",
+		"964fa0bf8c045097247fa0c973e0c167df08720409fd9e44546e0ceda3925f3e": "2.0.1",
+		"9626798cce6abd0f2ffef89f1a3d0092a60d34a837a02bbe571dbe00236a2c8c": "2.0.2",
+		"a768e5383990b512f9d4f97217eda94031c2fa4aea122585f5a475ab99dc7307": "2.1",
+		"0ad99a95ff637fc966fc4ce5fe1f9e78d3b24b113282f9990b95a6fde3383d9c": "2.3.1",
+		"a534961bbfce93966496f86c9314f46939fd082bb89986b48b7430c3bea903f7": "2.4",
+		"e8ffed196e04f81b015f847d4ec61f22f6731c11b5a21b1cfc45ccbc58b8ea45": "2.6",
+		"cee2305065bb61d434cdb45cfdaa46e7da148e5c6a7678d56f3e3dc8d7073eae": "2.7",
+		"66c89e2d5ae674641138858b571e65824df6873abb1677f7b2ef5c0dd4dbc442": "2.8",
+		"d4ec57440cd6db6eaf6bcb6b197f1cbaf5a3e26253d59578d51db307357cbf15": "2.8.2",
+		"0f038a1e0aa0aff76d66d1440c88a2b35a3d023ad8b2e3bac8e25a3208499f7e": "2.10.0",
+		"5c104d16ff9831b456e4d7eaf66bcf531f086767782d08eece3fb37e40467279": "2.12.0",
+		"edb797a8633f629b7c2187ccafd259a16a0b7b4cce4d42e646f8472358b8962a": "2.12.3",
+		"febbc7867784d0f06934fec59df55ee45f6b24c55b17fff71cc4fca80bf22ebb": "2.12.2",
+		"2b32bfc0556ea59307b9b2fde75b6dfbb5bf4f1d008d1402bc9a2357d8a8c61f": "2.13.0",
+		"84057480ba7da6fb6d9ea50c53a00848315833c1f34bf8f4a47f11a14499ae3f": "2.14.0",
+		"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855": "2.16.0b",
+		"ddad241274b834182525eeddc35c3198247507bd2df59645b58b94cd18fada7c": "2.17.0",
 	}
-	return "?"
-}
-
-const (
-	StatusOK       status = iota
-	StatusPatched
-	StatusUnknown
-	StatusMaybe
-	StatusVulnerable
 )
 
+var printMutex = new(sync.Mutex)
+
+
 type jar struct {
-	name       string
-	desc       []string
-	vulnerable bool
-	maybe      bool
-	patched    bool
+	name    string
+	version *version.Version
+	unknown bool
 }
 
 func (j *jar) check() {
 	if isDir, err := IsDirectory(j.name); err != nil {
-		j.desc = append(j.desc, err.Error())
-		j.setStatus(StatusUnknown)
+		j.unknown = true
+		if *debug {
+			fmt.Println(err.Error())
+		}
 	} else if isDir {
 		err := filepath.Walk(j.name,
 			func(osPathname string, info os.FileInfo, err error) error {
 				if strings.HasSuffix(osPathname, "log4j/core/lookup/JndiLookup.class") {
-					j.setStatus(StatusVulnerable)
-				} else if strings.HasSuffix(osPathname, "lookup/JndiLookup.class") {
-					j.setStatus(StatusMaybe)
-				} else if strings.HasSuffix(osPathname, "log4j/core/appender/mom/JmsAppender$Builder.class") {
 					err := func() error {
 						if size, err := FileSize(osPathname); err != nil {
-							return errors.New("cannot get size of jmsAppender")
+							return errors.New("cannot get size of JndiLookup.class")
 						} else if size > int64(math.Pow(2, 20)) {
-							return errors.New("jmsAppender is too big")
+							return errors.New("JndiLookup.class is too big")
 						}
 						if data, err := os.ReadFile(osPathname); err != nil {
 							return err
-						} else if bytes.Contains(data, []byte("allowedLdapHosts")) {
-							j.setStatus(StatusPatched)
+						} else {
+							hash := fmt.Sprintf("%x", sha256.Sum256(data))
+							if sVersion, ok := jndiLookupClassVersionHash[hash]; ! ok {
+								j.unknown = true
+								if *debug {
+									fmt.Printf("unknown version (hash %s) of JndiLookup.class for file %s\n", hash, j.name)
+								}
+							} else if jVersion, err := version.NewVersion(sVersion)	;err != nil {
+								j.unknown = true
+								if *debug {
+									fmt.Printf("error converting version %s: %e\n", sVersion, err)
+								}
+							} else {
+								j.setVersion(jVersion)
+							}
 						}
 						return nil
 					}()
 					if err != nil {
-						log.Printf("error reading %q: %v", osPathname, err)
+						j.unknown = true
+						if *debug {
+							log.Printf("error reading %q: %v", osPathname, err)
+						}
 					}
 				} else if osPathname == j.name {
-				} else if path.Ext(osPathname) == ".jar" {
+				} else if path.Ext(osPathname) == ".jar" || path.Ext(osPathname) == ".war" {
 					subJar := jar{name: osPathname}
 					subJar.check()
-					j.setStatus(subJar.getStatus())
+					j.setVersion(subJar.version)
 				}
 				return nil
 			})
@@ -95,13 +108,14 @@ func (j *jar) check() {
 	} else {
 		j.checkFile(j.name, nil, 0, 0)
 	}
-	j.printState()
 }
 
 func (j *jar) checkFile(pathToFile string, rd io.ReaderAt, size int64, depth int) {
 	if depth > 100 {
-		j.desc = append(j.desc, "reached recursion limit of 100 (why do you have so many jars in jars???)")
-		j.setStatus(StatusUnknown)
+		j.unknown = true
+		if *debug {
+			fmt.Printf("reached recursion limit of 100 (why do you have so many jars in jars???)")
+		}
 	}
 
 	if pathToFile == "" {
@@ -113,13 +127,19 @@ func (j *jar) checkFile(pathToFile string, rd io.ReaderAt, size int64, depth int
 			// #nosec G304
 			f, err := os.Open(pathToFile)
 			if err != nil {
-				j.desc = append(j.desc, err.Error())
+				j.unknown = true
+				if *debug {
+					fmt.Println(err.Error())
+				}
 				return err
 			}
 
 			stat, err := f.Stat()
 			if err != nil {
-				j.desc = append(j.desc, err.Error())
+				j.unknown = true
+				if *debug {
+					fmt.Println(err.Error())
+				}
 				return err
 			}
 
@@ -129,51 +149,51 @@ func (j *jar) checkFile(pathToFile string, rd io.ReaderAt, size int64, depth int
 
 		zipRd, err := zip.NewReader(rd, size)
 		if err != nil {
-			j.desc = append(j.desc, err.Error())
+			j.unknown = true
+			if *debug {
+				fmt.Println(err.Error())
+			}
 			return err
 		}
 
 		for _, file := range zipRd.File {
 			if strings.HasSuffix(file.Name, "log4j/core/lookup/JndiLookup.class") {
-				j.setStatus(StatusVulnerable)
-			}
-
-			if strings.HasSuffix(file.Name, "lookup/JndiLookup.class") {
-				j.setStatus(StatusMaybe)
-			}
-
-			if strings.HasSuffix(file.Name, "log4j/core/appender/mom/JmsAppender$Builder.class") {
-				err := func() error {
-					if file.UncompressedSize64 > 1024*1024 {
-						return errors.New("jmsAppender is too big")
-					}
-
-					subRd, err := file.Open()
-					if err != nil {
-						return err
-					}
-					defer subRd.Close()
-
-					data, err := io.ReadAll(subRd)
-					if err != nil {
-						return err
-					}
-
-					if bytes.Contains(data, []byte("allowedLdapHosts")) {
-						j.setStatus(StatusPatched)
-					}
-
-					return nil
-				}()
-				if err != nil {
-					log.Printf("error reading %q: %v", file.Name, err)
+				if file.UncompressedSize64 > uint64(math.Pow(2, 20)) {
+					return errors.New("JndiLookup.class is too big")
 				}
-			}
+				subRd, err := file.Open()
+				if err != nil {
+					return err
+				}
 
-			if path.Ext(file.Name) == ".jar" {
+				if data, err := io.ReadAll(subRd); err != nil {
+					return err
+				} else {
+					hash := fmt.Sprintf("%x", sha256.Sum256(data))
+					if sVersion, ok := jndiLookupClassVersionHash[hash]; ! ok {
+						_ = subRd.Close()
+						j.unknown = true
+						if *debug {
+							fmt.Printf("unknown version (hash %s) of JndiLookup.class for file %s\n", hash, j.name)
+						}
+					} else if jVersion, err := version.NewVersion(sVersion)	;err != nil {
+						_ = subRd.Close()
+						j.unknown = true
+						if *debug {
+							fmt.Printf("error converting version %s: %e\n", sVersion, err)
+						}
+					} else {
+						j.setVersion(jVersion)
+					}
+				}
+				_ = subRd.Close()
+				return nil
+			} else if path.Ext(file.Name) == ".jar" || path.Ext(file.Name) == ".war"  {
 				if file.UncompressedSize64 > 500*1024*1024 {
-					j.setStatus(StatusUnknown)
-					j.desc = append(j.desc, fmt.Sprintf("embedded jar file %q is too large (> 500 MB)", file.Name))
+					j.unknown = true
+					if *debug {
+						fmt.Printf("embedded jar file %q is too large (> 500 MB)", file.Name)
+					}
 				} else {
 					err := func() error {
 						subRd, err := file.Open()
@@ -181,10 +201,9 @@ func (j *jar) checkFile(pathToFile string, rd io.ReaderAt, size int64, depth int
 							return err
 						}
 
-						defer subRd.Close()
-
 						buf := bytes.NewBuffer(make([]byte, 0, file.UncompressedSize64))
 						_, err = buf.ReadFrom(subRd)
+						_ = subRd.Close()
 						if err != nil {
 							return err
 						}
@@ -193,74 +212,61 @@ func (j *jar) checkFile(pathToFile string, rd io.ReaderAt, size int64, depth int
 						return nil
 					}()
 					if err != nil {
-						j.setStatus(StatusUnknown)
-						j.desc = append(j.desc, fmt.Sprintf("error while checking embedded jar file %q: %v", file.Name, err))
+						j.unknown = true
+						if *debug {
+							fmt.Printf("error while checking embedded jar file %q: %v", file.Name, err)
+						}
 					}
 				}
 			}
 		}
 
-
 		return nil
 	}()
 	if err != nil {
-		j.setStatus(StatusUnknown)
-		j.desc = append(j.desc, err.Error())
-	}
-}
-
-func (j *jar) setStatus(newState status) {
-	switch newState {
-	case StatusVulnerable:
-		j.vulnerable = true
-	case StatusMaybe:
-		j.maybe = true
-	case StatusPatched:
-		j.patched = true
-	}
-}
-
-func (j *jar) getStatus() status {
-
-	if ! j.vulnerable {
-		if j.maybe {
-			return StatusMaybe
-		} else {
-			return StatusOK
+		j.unknown = true
+		if *debug {
+			fmt.Println(err.Error())
 		}
-	} else if j.patched {
-		return StatusPatched
-	} else {
-		return StatusVulnerable
 	}
 }
 
+func (j *jar) setVersion(newVersion *version.Version) {
+	if newVersion == nil {
+		return
+	}
+	if j.version == nil {
+		j.version = newVersion
+	}
+	if j.version.GreaterThan(newVersion) {
+		j.version = newVersion
+	}
+}
+
+func (j jar) getState() string {
+
+	if j.unknown {
+		return "UNKNOWN"
+	} else if j.version == nil {
+			return "OK"
+	} else {
+		return j.version.String() + "+"
+	}
+}
 func (j jar) printState() {
 	printMutex.Lock()
 	defer printMutex.Unlock()
 
-	if *mode == "list" {
-		if j.vulnerable || j.maybe {
-			fmt.Println(j.name)
-		}
-
+	jState := j.getState()
+	if jState == "OK" && ! *logOk {
 		return
 	}
 
-	jState := j.getStatus()
-	if jState == StatusOK && ! *logOk {
-			return
-	}
-
-	var desc string
-	if len(j.desc) > 0 {
-		desc = fmt.Sprintf(": %s", strings.Join(j.desc, ", "))
-	}
-	fmt.Printf("%-8.8s %s %s\n", jState.String(), j.name, desc)
+	fmt.Printf("%-10.10s %s\n", jState, j.name)
 }
 
-var mode = flag.String("mode", "report", "the output mode, either \"report\" (every jar pretty printed) or \"list\" (list of potentially vulnerable files)")
 var logOk = flag.Bool("ok", false, "also report OK lines")
+var debug = flag.Bool("debug", false, "print debug messages")
 
 func IsDirectory(path string) (bool, error) {
 	fileInfo, err := os.Stat(path)
@@ -304,11 +310,12 @@ func main() {
 
 	err := filepath.Walk(target,
 		func(osPathname string, info os.FileInfo, err error) error {
-			if filepath.Ext(osPathname) == ".jar" {
+			if filepath.Ext(osPathname) == ".jar" || filepath.Ext(osPathname) == ".war" {
 				j := jar{name: osPathname}
 				pool <- struct{}{}
 				go func() {
 					j.check()
+					j.printState()
 					<-pool
 				}()
 			}
